1e2eac7d6c3d22e0e0728da2f475fea78f0b9f75
mindstormUmar <umar.ali@mindstormstudios.com>
2017-06-12 22:10:12 +0500

    Remove extra folders

--- UFCW.Services/Models/Eligibility/Benifits/BenifitsServerResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-namespace UFCW.Services.Models.Eligibility.Benifits
-{
-    public class BenifitsServerResponse
-    {
-        List<Benifits> UserBenifits { get; set;}
-    }
-}
