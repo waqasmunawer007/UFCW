dd276beaa73f1fe4236f957a65bf90b21f1ec770
waqar <waqar.ahmed5571@gmail.com>
2017-08-02 22:00:13 +0500

    Visit URL Button ShowOrHide achieved here

---
+++ UFCW/Converters/ShowHideVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UFCW.Converters
+{
+   public class ShowHideVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            List<Link> list = new List<Link>();
+            list = value as List<Link>;
+            if (list.Count>0) //if there is any single Link entry available then show the visibility of Visit button 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
