7008956f43098a1ae2e1ecb74bc558d48a7f23a8
Umar Ali <umar.ali@mindstormstudios.com>
2017-10-02 21:08:24 +0500

    Add inbox funtionality TBC

---
+++ UFCW.Droid/Renderers/BorderedEditorRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UFCW.Droid;
+using UFCW.Views.CustomView;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(BorederedEditor), typeof(EditorRenderer))]
+namespace UFCW.Droid.Renderers
+{
+    public class BorderedEditorRenderer: EditorRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+		{
+			base.OnElementChanged(e);
+			if (Control != null)
+			{
+				Control.Background = Resources.GetDrawable(Resource.Drawable.EditTextProperties);
+
+			}
+		}
+    }
+}
---
+++ UFCW/Views/CustomView/BorederedEditor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace UFCW.Views.CustomView
+{
+    public class BorederedEditor: Editor
+    {
+        public BorederedEditor()
+        {
+        }
+    }
+}
